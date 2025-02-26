d1cfa47f67ee004aaf295a15311e23ca20091b44
Bens <Jefferson.b3ntz@gmail.com>
2025-02-26 11:05:22 -0300

    javascript da FAQs pronto

---
+++ js/detailReveal.js
@@ -0,0 +1,69 @@
+var plus1 = document.querySelector("#buttonPlus1");
+var minus1 = document.querySelector("#buttonMinus1");
+var answer1 = document.querySelector("#awnser1");
+
+plus1.addEventListener("click", () => {
+    answer1.style.display = "block";
+    plus1.style.display = "none";
+    minus1.style.display = "block";
+});
+
+minus1.addEventListener("click", () => {
+    answer1.style.display = "none";
+    plus1.style.display = "block";
+    minus1.style.display = "none";
+});
+
+// --------------------------------------
+
+var plus2 = document.querySelector("#buttonPlus2");
+var minus2 = document.querySelector("#buttonMinus2");
+var answer2 = document.querySelector("#awnser2");
+
+plus2.addEventListener("click", () => {
+    answer2.style.display = "block";
+    plus2.style.display = "none";
+    minus2.style.display = "block";
+});
+
+minus2.addEventListener("click", () => {
+    answer2.style.display = "none";
+    plus2.style.display = "block";
+    minus2.style.display = "none";
+});
+
+// --------------------------------------
+
+var plus3 = document.querySelector("#buttonPlus3");
+var minus3 = document.querySelector("#buttonMinus3");
+var answer3 = document.querySelector("#awnser3");
+
+plus3.addEventListener("click", () => {
+    answer3.style.display = "block";
+    plus3.style.display = "none";
+    minus3.style.display = "block";
+});
+
+minus3.addEventListener("click", () => {
+    answer3.style.display = "none";
+    plus3.style.display = "block";
+    minus3.style.display = "none";
+});
+
+// --------------------------------------
+
+var plus4 = document.querySelector("#buttonPlus4");
+var minus4 = document.querySelector("#buttonMinus4");
+var answer4 = document.querySelector("#awnser4");
+
+plus4.addEventListener("click", () => {
+    answer4.style.display = "block";
+    plus4.style.display = "none";
+    minus4.style.display = "block";
+});
+
+minus4.addEventListener("click", () => {
+    answer4.style.display = "none";
+    plus4.style.display = "block";
+    minus4.style.display = "none";
+});
