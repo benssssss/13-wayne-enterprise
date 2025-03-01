ffa87f2313c34afdc8813824c77d311e8005b6ef
Bens <Jefferson.b3ntz@gmail.com>
2025-03-01 11:04:42 -0300

    codigo 320px pronto

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const carouselItems = document.querySelectorAll('.carouselItem');
-    let currentIndex = 0;
-  
-    function showSlide(index) {
-      carouselItems.forEach(item => {
-        item.style.display = 'none';
-      });
-      carouselItems[index].style.display = 'block';
-    }
-  
-    function nextSlide() {
-      currentIndex = (currentIndex + 1) % carouselItems.length;
-      showSlide(currentIndex);
-    }
-  
-    function previousSlide() {
-      currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
-      showSlide(currentIndex);
-    }
-  
-    // Show the first slide initially
+  const carouselItems = document.querySelectorAll('.carouselItem');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    carouselItems.forEach(item => {
+      item.style.display = 'none';
+    });
+    carouselItems[index].style.display = 'block';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
     showSlide(currentIndex);
-  
-    // Set up event listeners for next and previous buttons
-    document.getElementById('nextBtn').addEventListener('click', nextSlide);
-    document.getElementById('prevBtn').addEventListener('click', previousSlide);
-  });
+  }
+
+  function previousSlide() {
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+
+  showSlide(currentIndex);
+
+  document.getElementById('nextBtn').addEventListener('click', nextSlide);
+  document.getElementById('prevBtn').addEventListener('click', previousSlide);
+});
